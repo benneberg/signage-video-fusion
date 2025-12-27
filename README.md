1c37ed84039f0328e8b5dd80f04d53c4c65c1a27
Lukas Benneberg <lukas.benneberg@dise.com>
2025-12-27 14:22:47 +0100

    Initial commit

--- README.md
+++ README.md
@@ -115,7 +115,7 @@ Playlists use a JSON schema for import/export:
 
 MIT License
 
-Copyright (c) 2024
+Copyright (c) 2026
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: (process.env.BASE_URL ?? "/").endsWith("/")
+    ? (process.env.BASE_URL ?? "/")
+    : `${process.env.BASE_URL ?? "/"}/`,
   server: {
     host: "::",
     port: 8080,
